89f988310ed2bcb3ae719b39550f88bc49407bc9
shivaansh27 <shivanshsharma2704@gmail.com>
2025-12-18 10:29:26 +0530

    DSA

---
+++ FirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+class Area
+{
+    public static void Main()
+    {
+        Console.Write("Enter radius: ");
+        double r= Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * r * r;
+        Console.WriteLine("Area of circle is: "+area);
+
+    }
+}
