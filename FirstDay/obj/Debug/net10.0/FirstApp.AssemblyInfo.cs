05ce80122138a7a2a76cd469fc1593fc7fcdb0cc
shivaansh27 <shivanshsharma2704@gmail.com>
2026-01-29 15:06:10 +0530

    done

--- FirstDay/obj/Debug/net10.0/FirstApp.AssemblyInfo.cs
+++ FirstDay/obj/Debug/net10.0/FirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf964edf09c9d124a80c1b8e407c6cc3023a1c87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f7f96ac5a8e545153ff128e466894fe779cff33")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TopBrain/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Marks { get; set; }
+}
+
+class StudentComparer : IComparer<Student>
+{
+    public int Compare(Student x, Student y)
+    {
+        int marksCompare = y.Marks.CompareTo(x.Marks);
+        if (marksCompare != 0)
+            return marksCompare;
+
+        return x.Age.CompareTo(y.Age);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student { Name = "Jyoti", Age = 13, Marks = 98 },
+            new Student { Name = "Deepak", Age = 12, Marks = 98 },
+            new Student { Name = "Himanshu", Age = 14, Marks = 85 }
+        };
+
+        students.Sort(new StudentComparer());
+
+        foreach (var s in students)
+        {
+            Console.WriteLine($"{s.Name} {s.Age} {s.Marks}");
+        }
+    }
+}
